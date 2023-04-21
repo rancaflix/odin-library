4693969b66bfad877662646ad42c1e7d1c9d5b1e
rancaflix <cristobalpozom@gmail.com>
2023-04-21 00:03:27 -0400

    Functionalities added

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const pagesInput = document.getElementById("pages-input");
 const readStatus = document.getElementById("read-status");
 const formSubmitButton = document.getElementById("form-button");
 const formPopup = document.getElementById("form-popup");
+const titleEditInput = document.getElementById('edit-title');
+const authorEditInput = document.getElementById('edit-author');
+const pagesEditInput = document.getElementById('edit-pages');
+const editModule = document.getElementById("edit-module-wrapper");
+const editBookForm = document.getElementById("edit-book-form");
+const editSubmitButton = document.getElementById("submit-edit-btn")
 const myLibrary = [];
 
 // Book prototype
@@ -32,7 +38,8 @@ function displayLibrary() {
   // only display the last added book
   displayCard(myLibrary[lastBookIndex], lastBookIndex, cardArea);
 }
-  
+
+// create a card for each book
 function displayCard(book, index, cardArea) {
   // create a card only for the specified book
   const card = document.createElement('div');
@@ -40,23 +47,23 @@ function displayCard(book, index, cardArea) {
   
   const title = document.createElement('h2');
   title.textContent = book.title;
+  title.setAttribute('id', 'titleDisplay');
   card.appendChild(title);
   
   const author = document.createElement('p');
   author.textContent = `Author: ${book.author}`;
+  author.setAttribute('id', 'authorDisplay');
   card.appendChild(author);
   
   const pages = document.createElement('p');
-  pages.textContent = `Pages: ${book.pages}`;
+  pages.textContent = `${book.pages}p`;
+  pages.setAttribute('id', 'pagesDisplay');
   card.appendChild(pages);
   
+  // card components
   const readContainer = document.createElement('div');
   readContainer.classList.add('read-container');
   card.appendChild(readContainer);
-
-  const readLabel = document.createElement('label');
-  readLabel.textContent = 'Read:';
-  readContainer.appendChild(readLabel);
 
   const readSlider = document.createElement('input');
   readSlider.type = 'range';
@@ -64,24 +71,31 @@ function displayCard(book, index, cardArea) {
   readSlider.max = 1;
   readSlider.step = 1;
   readSlider.value = book.read ? 1 : 0;
+  readSlider.classList.add('slider');
   readContainer.appendChild(readSlider);
 
   const readStatus = document.createElement('span');
   readStatus.classList.add('read-status');
-  readStatus.textContent = book.read ? 'Yes' : 'No';
+  readStatus.textContent = book.read ? 'Read' : 'Not Read';
   readContainer.appendChild(readStatus);
   
+  // card buttons 
   const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('id', 'cardBtnContainer');
   card.appendChild(buttonContainer);
   
   const editButton = document.createElement('button');
   editButton.classList.add('edit-button');
   editButton.textContent = 'Edit';
   editButton.addEventListener('click', () => {
-    // implement edit book functionality
+    populateEditForm(book);
+    editBookForm.setAttribute('data-book-index', myLibrary.indexOf(book));
+    editModule.style.display = 'block';
+    editBookForm.style.display = 'block';
+    overlay.style.display = 'block';
   });
   buttonContainer.appendChild(editButton);
- 
+
   const deleteButton = document.createElement('button');
   deleteButton.classList.add('delete-button');
   deleteButton.textContent = 'Delete';
@@ -103,7 +117,7 @@ function displayCard(book, index, cardArea) {
   // add event listener to update read status when slider is changed
   readSlider.addEventListener('input', () => {
     book.read = readSlider.value == 1;
-    readStatus.textContent = book.read ? 'Yes' : 'No';
+    readStatus.textContent = book.read ? 'Read' : 'Not Read';
     if (book.read) {
       card.classList.add('card-read');
     } else {
@@ -111,25 +125,6 @@ function displayCard(book, index, cardArea) {
     }
   });
 }
-
-
-function openForm() {
-    const addBookForm = document.querySelector("#add-book-form");
-    addBookForm.style.display = "block";
-    const overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-  }
-
-newBookBtn.addEventListener('click', () => {
-    formPopup.style.display = 'block';
-    overlay.style.display = 'block';
-});
-
-overlay.addEventListener('click', () => {
-    formPopup.style.display = 'none';
-    overlay.style.display = 'none';
-});
-
 
 function createBook() {
   // check if required fields are empty
@@ -146,13 +141,54 @@ function createBook() {
   titleInput.value = "";
   authorInput.value = "";
   pagesInput.value = "";
-  readStatus.checked = false;
-  
-  // hide form
-  const addBookForm = document.querySelector("#add-book-form");
-  addBookForm.style.display = "none";
+  readStatus.checked = false;  
 }
-  
+
+function populateEditForm(book) {
+  titleEditInput.value = book.title;
+  authorEditInput.value = book.author;
+  pagesEditInput.value = book.pages;
+  // readEditStatus.value = book.read ? 1 : 0;
+  // set data-book-index attribute on form to the index of the book in the library
+  editBookForm.setAttribute('data-book-index', myLibrary.indexOf(book));
+}
+
+function closeEditForm() {
+  editBookForm.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+ function updateBook() {
+  const index = parseInt(editBookForm.getAttribute('data-book-index'));
+  const book = myLibrary[index];
+
+  // update book properties with form values
+  book.title = titleEditInput.value;
+  book.author = authorEditInput.value;
+  book.pages = pagesEditInput.value;
+  // book.read = readEditStatus.checked;
+
+  // update card display with new values
+  const cardArea = document.getElementById('card-area');
+  const card = cardArea.children[index];
+  card.children[0].textContent = book.title;
+  card.children[1].textContent = `Author: ${book.author}`;
+  card.children[2].textContent = `${book.pages}p`;
+  // card.children[3].textContent = `Read: ${book.read ? 'Yes' : 'No'}`; THIS IS MESSING WITH SLIDER
+  closeEditForm();
+}
+
+newBookBtn.addEventListener('click', () => {
+  formPopup.style.display = 'block';
+  overlay.style.display = 'block';
+
+});
+
+overlay.addEventListener('click', () => {
+  formPopup.style.display = 'none';
+  overlay.style.display = 'none';
+  editBookForm.style.display = 'none';
+});
 
 formSubmitButton.addEventListener(
   "click",
@@ -163,9 +199,13 @@ formSubmitButton.addEventListener(
     authorInput.value = "";
     pagesInput.value = "";
     readStatus.checked = false;
-    openForm();
   },
   false
 );
 
-
+editSubmitButton.addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+    updateBook();
+  });
